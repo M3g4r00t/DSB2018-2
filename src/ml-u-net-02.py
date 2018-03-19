cfbeeaaa9d10752549c9aedff7b611ad448a2cb9
M3g4r00t <dennys.mallqui@gmail.com>
2018-03-19 18:15:32 -0300

    Merge

--- src/ml-u-net-02.py
+++ src/ml-u-net-02.py
@@ -15,22 +15,35 @@ from keras.layers.core import Dropout, Lambda
 from keras.layers.merge import concatenate
 from keras.layers.pooling import MaxPooling2D
 from keras.models import Model, load_model
-from skimage.io import imread, imshow
+from keras.preprocessing import image
+from skimage.io import imread, imshow, imsave
 from skimage.morphology import label
 from skimage.transform import resize
+from sklearn import model_selection
 from tqdm import tqdm
 
-# Set some parameters
+# Image dimensions (resize)
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 IMG_CHANNELS = 3
-BATCH_SIZE = 16  # the higher the better
-ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')) + '\\'
+
+# Batch size for weight calculation
+BATCH_SIZE = 16
+
+# Filename setup
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '\\'
 TRAIN_PATH = ROOT_PATH + 'input\\stage1_train_aug_02\\'
 TEST_PATH = ROOT_PATH + 'input\\stage1_test\\'
+MODEL_FILE_NAME = 'model-dsbowl2018-4.h5'
+MODEL_FILE_PATH = ROOT_PATH + 'output\\' + MODEL_FILE_NAME
+VIEW_IMAGE_FLAG = True
+SAVE_IMAGE_RESULTS = True
 
+# Skimage warnings
 warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
-input_seed = 42
+
+# For experiment replication
+input_seed = 42  # the universe response
 random.seed = input_seed
 np.random.seed(input_seed)
 
@@ -38,9 +51,15 @@ np.random.seed(input_seed)
 train_ids = next(os.walk(TRAIN_PATH))[1]
 test_ids = next(os.walk(TEST_PATH))[1]
 
+# Load input data
 # Get and resize train images and masks
 X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
 Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+''' Begin: Erase in production '''
+X_train = X_train[:32 * 4]
+Y_train = Y_train[:32 * 4]
+train_ids = train_ids[:32 * 4]
+''' End: Erase in production '''
 print('Getting train images and masks ... ')
 sys.stdout.flush()
 for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
@@ -62,62 +81,27 @@ for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
     img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
     X_test[n] = img
 
-# Check if training data looks all right
-ix = random.randint(0, len(train_ids))
-imshow(X_train[ix])
-plt.draw()
-imshow(np.squeeze(Y_train[ix]))
-plt.draw()
+X_train, X_val, Y_train, Y_val = model_selection.train_test_split(X_train, Y_train, test_size=0.25,
+                                                                  random_state=input_seed)
 
-# Data augmentation
-
-print('Data augmentation for validation ...')
-
-from keras.preprocessing import image
-
-# Creating the training Image and Mask generator
-image_datagen = image.ImageDataGenerator(shear_range=0.5, rotation_range=50, zoom_range=0.2, width_shift_range=0.2,
-                                         height_shift_range=0.2, fill_mode='reflect')
-mask_datagen = image.ImageDataGenerator(shear_range=0.5, rotation_range=50, zoom_range=0.2, width_shift_range=0.2,
-                                        height_shift_range=0.2, fill_mode='reflect')
-
-# Keep the same seed for image and mask generators so they fit together
-
-image_datagen.fit(X_train[:int(X_train.shape[0] * 0.75)], augment=True, seed=input_seed)
-mask_datagen.fit(Y_train[:int(Y_train.shape[0] * 0.75)], augment=True, seed=input_seed)
-
-x = image_datagen.flow(X_train[:int(X_train.shape[0] * 0.75)], batch_size=BATCH_SIZE, shuffle=True, seed=input_seed)
-y = mask_datagen.flow(Y_train[:int(Y_train.shape[0] * 0.75)], batch_size=BATCH_SIZE, shuffle=True, seed=input_seed)
-
-# Creating the validation Image and Mask generator
-image_datagen_val = image.ImageDataGenerator()
-mask_datagen_val = image.ImageDataGenerator()
-
-image_datagen_val.fit(X_train[int(X_train.shape[0] * 0.75):], augment=True, seed=input_seed)
-mask_datagen_val.fit(Y_train[int(Y_train.shape[0] * 0.75):], augment=True, seed=input_seed)
-
-x_val = image_datagen_val.flow(X_train[int(X_train.shape[0] * 0.75):], batch_size=BATCH_SIZE, shuffle=True,
-                               seed=input_seed)
-y_val = mask_datagen_val.flow(Y_train[int(Y_train.shape[0] * 0.75):], batch_size=BATCH_SIZE, shuffle=True,
-                              seed=input_seed)
-
-# Checking if the images fit
-
-imshow(x.next()[0].astype(np.uint8))
-plt.draw()
-imshow(np.squeeze(y.next()[0].astype(np.uint8)))
-plt.draw()
-imshow(x_val.next()[0].astype(np.uint8))
-plt.draw()
-imshow(np.squeeze(y_val.next()[0].astype(np.uint8)))
-plt.draw()
-
-# creating a training and validation generator that generate masks and images
-train_generator = zip(x, y)
-val_generator = zip(x_val, y_val)
+if VIEW_IMAGE_FLAG:
+    try:
+        # Check if training data looks all right
+        ix = random.randint(0, len(X_train))
+        imshow(X_train[ix])
+        plt.show()
+        imshow(np.squeeze(Y_train[ix]))
+        plt.show()
+        ix = random.randint(0, len(X_val))
+        imshow(X_val[ix])
+        plt.show()
+        imshow(np.squeeze(Y_val[ix]))
+        plt.show()
+    except Exception as e:
+        print("Unexpected error:", e.__str__())
 
 
-# Define IoU metric
+# Define IoU metric: IoU = (A Intersection B)/(A union B)
 def mean_iou(y_true, y_pred):
     prec = []
     for t in np.arange(0.5, 1.0, 0.05):
@@ -129,81 +113,129 @@ def mean_iou(y_true, y_pred):
         prec.append(score)
     return K.mean(K.stack(prec), axis=0)
 
-print('Build network ...')
 
-# Build U-Net model
-inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-s = Lambda(lambda x: x / 255)(inputs)
+if not os.path.exists(MODEL_FILE_PATH):
+    # Data augmentation
 
-c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(s)
-c1 = Dropout(0.1)(c1)
-c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c1)
-p1 = MaxPooling2D((2, 2))(c1)
+    print('Data augmentation for validation ...')
 
-c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p1)
-c2 = Dropout(0.1)(c2)
-c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c2)
-p2 = MaxPooling2D((2, 2))(c2)
+    # Creating the training Image and Mask generator
+    image_datagen = image.ImageDataGenerator(shear_range=0.5, rotation_range=50, zoom_range=0.2, width_shift_range=0.2,
+                                             height_shift_range=0.2, fill_mode='reflect')
+    mask_datagen = image.ImageDataGenerator(shear_range=0.5, rotation_range=50, zoom_range=0.2, width_shift_range=0.2,
+                                            height_shift_range=0.2, fill_mode='reflect')
 
-c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p2)
-c3 = Dropout(0.2)(c3)
-c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c3)
-p3 = MaxPooling2D((2, 2))(c3)
+    # Keep the same seed for image and mask generators so they fit together
 
-c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p3)
-c4 = Dropout(0.2)(c4)
-c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c4)
-p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+    image_datagen.fit(X_train, augment=True, rounds=3, seed=input_seed)
+    mask_datagen.fit(Y_train, augment=True, rounds=3, seed=input_seed)
 
-c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p4)
-c5 = Dropout(0.3)(c5)
-c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c5)
+    x = image_datagen.flow(X_train, batch_size=BATCH_SIZE, shuffle=True, seed=input_seed)
+    y = mask_datagen.flow(Y_train, batch_size=BATCH_SIZE, shuffle=True, seed=input_seed)
 
-u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-u6 = concatenate([u6, c4])
-c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u6)
-c6 = Dropout(0.2)(c6)
-c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c6)
+    # Creating the validation Image and Mask generator
+    image_datagen_val = image.ImageDataGenerator()
+    mask_datagen_val = image.ImageDataGenerator()
 
-u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-u7 = concatenate([u7, c3])
-c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u7)
-c7 = Dropout(0.2)(c7)
-c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c7)
+    image_datagen_val.fit(X_val, augment=True, seed=input_seed)
+    mask_datagen_val.fit(Y_val, augment=True, seed=input_seed)
 
-u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-u8 = concatenate([u8, c2])
-c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u8)
-c8 = Dropout(0.1)(c8)
-c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c8)
+    x_val = image_datagen_val.flow(X_val, batch_size=BATCH_SIZE, shuffle=True, seed=input_seed)
+    y_val = mask_datagen_val.flow(Y_val, batch_size=BATCH_SIZE, shuffle=True, seed=input_seed)
 
-u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u9)
-c9 = Dropout(0.1)(c9)
-c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c9)
+    if VIEW_IMAGE_FLAG:
+        try:
+            # Checking if the images fit
+            imshow(x.next()[0].astype(np.uint8))
+            plt.show()
+            imshow(np.squeeze(y.next()[0].astype(np.uint8)))
+            plt.show()
+            imshow(x_val.next()[0].astype(np.uint8))
+            plt.show()
+            imshow(np.squeeze(y_val.next()[0].astype(np.uint8)))
+            plt.show()
+        except Exception as e:
+            print("Unexpected error:", e.__str__())
 
-outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    # creating a training and validation generator that generate masks and images
+    train_generator = zip(x, y)
+    val_generator = zip(x_val, y_val)
 
-model = Model(inputs=[inputs], outputs=[outputs])
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
-model.summary()
+    print('Build network ...')
 
-# Fit model
-earlystopper = EarlyStopping(patience=10, verbose=1, mode='min')
-checkpointer = ModelCheckpoint(ROOT_PATH + 'output\\model-dsbowl2018-4.h5', verbose=1, save_best_only=True)
+    # Build U-Net model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = Lambda(lambda x: x / 255)(inputs)
 
-if not os.path.exists(ROOT_PATH + 'output\\model-dsbowl2018-4.h5'):
+    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+    model.summary()
+
+    # Fit model
+    earlystopper = EarlyStopping(patience=10, verbose=1, mode='min')
+    checkpointer = ModelCheckpoint(MODEL_FILE_PATH, verbose=1, save_best_only=True)
+
     print('Train network')
     results = model.fit_generator(train_generator, validation_data=val_generator, validation_steps=BATCH_SIZE,
                                   steps_per_epoch=250,
                                   epochs=200, callbacks=[earlystopper, checkpointer])
 
-print('Reading model > ' + 'output\\model-dsbowl2018-4.h5')
+print('Reading model > ' + MODEL_FILE_PATH)
 # Predict on train, val and test
-model = load_model(ROOT_PATH + 'output\\model-dsbowl2018-4.h5', custom_objects={'mean_iou': mean_iou})
-preds_train = model.predict(X_train[:int(X_train.shape[0] * 0.9)], verbose=1)
-preds_val = model.predict(X_train[int(X_train.shape[0] * 0.9):], verbose=1)
+model = load_model(MODEL_FILE_PATH, custom_objects={'mean_iou': mean_iou})
+print(model.summary())
+preds_train = model.predict(X_train, verbose=1)
+preds_val = model.predict(X_val, verbose=1)
 preds_test = model.predict(X_test, verbose=1)
 
 # Threshold predictions
@@ -218,23 +250,35 @@ for i in range(len(preds_test)):
                                        (sizes_test[i][0], sizes_test[i][1]),
                                        mode='constant', preserve_range=True))
 
-# Perform a sanity check on some random training samples
-ix = random.randint(0, len(preds_train_t))
-imshow(X_train[ix])
-plt.draw()
-imshow(np.squeeze(Y_train[ix]))
-plt.draw()
-imshow(np.squeeze(preds_train_t[ix]))
-plt.draw()
+if VIEW_IMAGE_FLAG:
+    try:
+        # Perform a sanity check on some random training samples
+        ix = random.randint(0, len(preds_train_t))
+        imshow(X_train[ix])
+        plt.show()
+        imshow(np.squeeze(Y_train[ix]))
+        plt.show()
+        imshow(np.squeeze(preds_train_t[ix].astype(np.uint8)))
+        plt.show()
 
-# Perform a sanity check on some random validation samples
-ix = random.randint(0, len(preds_val_t))
-imshow(X_train[int(X_train.shape[0] * 0.75):][ix])
-plt.draw()
-imshow(np.squeeze(Y_train[int(Y_train.shape[0] * 0.75):][ix]))
-plt.draw()
-imshow(np.squeeze(preds_val_t[ix]))
-plt.draw()
+        # Perform a sanity check on some random validation samples
+        ix = random.randint(0, len(preds_val_t))
+        imshow(X_val[ix])
+        plt.show()
+        imshow(np.squeeze(Y_val[ix].astype(np.uint8)))
+        plt.show()
+        imshow(np.squeeze(preds_val_t[ix].astype(np.uint8)))
+        plt.show()
+
+        # Perform a sanity check on some random validation samples
+        ix = random.randint(0, len(preds_test_t))
+        imshow(X_test[ix])
+        plt.show()
+        imshow(np.squeeze(preds_test_t[ix].astype(np.uint8)))
+        plt.show()
+
+    except Exception as e:
+        print("Unexpected error:", e.__str__())
 
 
 # Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
@@ -263,19 +307,17 @@ for n, id_ in enumerate(test_ids):
     rles.extend(rle)
     new_test_ids.extend([id_] * len(rle))
 
-# Perform a sanity check on some random validation samples
-ix = random.randint(0, len(preds_test_t))
-imshow(X_test[ix])
-plt.draw()
-imshow(np.squeeze(preds_test_t[ix]))
-plt.draw()
-
 print('Create submission DataFrame ...')
 
 # Create submission DataFrame
 sub = pd.DataFrame()
 sub['ImageId'] = new_test_ids
 sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
-sub.to_csv(ROOT_PATH + 'output\\sub-dsbowl2018-aug-u-net-02-wo-norm.csv', index=False)
+sub.to_csv(ROOT_PATH + 'output\\sub-dsbowl2018-' + MODEL_FILE_NAME + '-20180319.csv', index=False)
 
-plt.show()
+if SAVE_IMAGE_RESULTS:
+    for n, id_ in enumerate(test_ids):
+        path = TEST_PATH + id_
+        imsave(path + '/images/' + MODEL_FILE_NAME + '.png', resize(np.squeeze(preds_test_t[n]),
+                                                                    (sizes_test[n][0], sizes_test[n][1]),
+                                                                    mode='constant', preserve_range=True))
